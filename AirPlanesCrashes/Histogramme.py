6b3b911b23dca61fa05e7b83a2bb4f405e735227
Killianstr <stricot.killian@gmail.com>
2019-12-04 10:15:27 +0100

    Open csv

--- AirPlanesCrashes/Histogramme.py
+++ AirPlanesCrashes/Histogramme.py
@@ -1,1 +1,25 @@
+import matplotlib.pyplot as plt
+import csv
 
+liste_dictionnaire = {}
+
+fichier = open('../Airplane_Crashes_and_Fatalities_Since_1908.csv',mode = 'r', encoding = 'utf8')
+liste_dictionnaire = csv.DictReader(fichier)
+       
+
+def Init_limits():
+    # for value in liste_dictionnaire["Date"]:
+    #     date_min = min(value)
+    # print(date_min)
+    for row in liste_dictionnaire:
+        print(row)
+
+
+def Close_csv():
+    fichier.close()
+
+print(liste_dictionnaire)
+for row in liste_dictionnaire:
+        print(row)
+
+#Close_csv()
