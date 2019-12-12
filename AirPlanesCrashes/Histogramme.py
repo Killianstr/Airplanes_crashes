b6acc9d54942709bedad8f5f251abb897054b0ff
Killianstr <stricot.killian@gmail.com>
2019-12-12 23:46:14 +0100

    travail killian

--- AirPlanesCrashes/Histogramme.py
+++ AirPlanesCrashes/Histogramme.py
@@ -1,43 +1,65 @@
 import matplotlib.pyplot as plt
 import csv
-from datetime import date, time, datetime
 from collections import Counter
+import numpy as np
+
+#récupère l'année
 def Date_rework(date_string):
-    # new_date = date_string[-4:] + '/' + date_string[0:2] + '/' + date_string[3:5]
     new_date = date_string[-4:]
     return new_date
 
-liste_dictionnaire = {}
+#crée un histogramme et l'enregistre au format png
+def Create_histo(liste_used,color,yaxe,ylab,num):
+    plt.bar(list_annees,liste_used, color = color)
+    plt.xlabel('Années')    
+    axes.xaxis.set_ticks(np.arange(0,100,9))
+    axes.yaxis.set_ticks(yaxe)
+    plt.ylabel(ylab)
+    plt.title(ylab + 'en fonction des années')
+    plt.show()
+    plt.savefig('histo'+num+'.png')
 
 fichier = open('../Airplane_Crashes_and_Fatalities_Since_1908.csv',mode = 'r', encoding = 'utf8')
-liste_dictionnaire = csv.DictReader(fichier)
+reader = csv.DictReader(fichier)
 
+liste_dictionnaire = list(reader)
 
+copy_csv = liste_dictionnaire[:]
 list_date = []
+list_annees = []
+nombre_morts = []
+nombre_survived = []
+nombre_abord = []
+
+#Récupère les valeurs à traiter dans des listes
 for value in liste_dictionnaire:
-        list_date.append(Date_rework(value.get('Date')))
+    list_annees.append(Date_rework(value.get('Date')))
+    list_date.append(value.get('Date'))
+    nombre_morts.append(value.get('Fatalities'))
+    nombre_survived.append(value.get('Ground'))
+    nombre_abord.append(value.get('Aboard'))
 
-dict_date = Counter(list_date)
-# dict_nbcrash = {}
-# for value in list_date : 
-#     if value in dict_nbcrash.keys :
-#         dict_nbcrash.
+dict_date = Counter(list_annees)
 
-def Init_limits():
+#Pour trouver le max et le min d'une liste
+def Init_limits(l):
     value = []
-    value = [min(list_date),max(list_date)]
-    # print(max(list_date))
+    value = [min(l),max(l)]
     return value
 
+axes = plt.gca()
 
-print(liste_dictionnaire.fieldnames)
-min_max = Init_limits()
-
-plt.hist(list_date,range = (min_max[0],min_max[1]), bins = 10, color = 'grey',
-            edgecolor = 'blue')
-plt.xlabel('Années')
+#cree l'histogramme qui prend un seul paramètre 
+plt.hist(list_annees,bins = 30, color = 'grey')
+plt.xlabel('Années')    
+axes.xaxis.set_ticks(np.arange(0,100,9))
+axes.yaxis.set_ticks(np.arange(0,350,30))
 plt.ylabel('Nombre de crash')
 plt.title('Nombre de crash en fonction des années')
 plt.show()
+plt.savefig('histo1.png')
 
-#Close_csv()
+#créé les 3 autres
+Create_histo(nombre_morts,'r',np.arange(0,200,30),'Nombre de morts ','2')
+Create_histo(nombre_survived,'blue',np.arange(0,50,10),'Nombre de survivants ','3')
+Create_histo(nombre_abord,'yellow',np.arange(0,200,30),'Nombre de personnes à bord ','4')
